eca267feed2f0676b86f1935519ef55af43b86db
Rajaeirq <rajae-irq@hotmail.fr>
2023-03-16 19:30:01 -0700

    task 5 trying again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 
-/''
-' main - print the string in the put function
-'
-Description: using the main function with proper grammar, but the outcome is a piece of art,
-' Return: 0
-'/
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints with proper grammar, but the outcome is a piece of art,
+* return: (0)
+*/
 int main(void)
-(
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return return (0);
-)
+	return(0);
+}
