81be132996eff1a66b2e05ae6837fc943d4fbc28
Rajaeirq <rajae-irq@hotmail.fr>
2023-03-16 19:54:55 -0700

    trying the advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - print quote
+*
+* Description: print Dora's quote
+* Return: Always 1 (Success)
+*/
+
+inter main(void)
+{
+write (2, "and that piece of art is useful" - Dora Korpar, 2015-10-1n", 59);
+return (1);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main function
+* description: using the main function
 * this program prints with proper grammar, but the outcome is a piece of art,
-* return: (0)
+* return: always (0) (success)
 */
 int main(void)
 {
