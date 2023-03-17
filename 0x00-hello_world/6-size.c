35681aea16b33d536cf6d09af5f316f02da8fdfa
Rajaeirq <rajae-irq@hotmail.fr>
2023-03-16 18:38:11 -0700

    task 6 correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,22 @@
-#!/bin/bash
-#include <stdio.h>
-
-/"
-' You should produce the exact same output as in the example
-' Warnings are allowed
-'
-' Your program should return 0
-'/
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 (
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f ;
+        char c;
+        int i;
+        long li;
+        long long lli;
+        float f;
 
-	printf("size of a char: %ld byte(s)\n", sizeof(c));
-	printf(""size of a int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long: %ld byte(s)\n", sizeof(li));
-	printf("size of a long long: %ld byte(s)\n", sizeof(lli));
-	printf("size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
+        printf("size of a char: %ld byte(s)\n", sizeof(c));
+        printf("size of an int: %ld byte(s)\n", sizeof(i));
+        printf("size of a long int: %ld byte(s)\n", sizeof(li));
+        printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
+        printf("size of a float: %ld byte(s)\n", sizeof(f));
+        return (0);
 )
