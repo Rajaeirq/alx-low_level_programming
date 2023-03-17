e566b652128dbc00b53fe59be79463f29b761e42
Rajaeirq <rajae-irq@hotmail.fr>
2023-03-16 18:12:31 -0700

    C task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/''
+' main - print the string in the put function
+'
+Description: using the main function with proper grammar, but the outcome is a piece of art,
+' Return: 0
+'/
+int main(void)
+(
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return return (0);
+)
