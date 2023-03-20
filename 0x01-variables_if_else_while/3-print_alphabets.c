9dab7c8997b666adcabab0018a07d1524982abe0
Rajaeirq <rajae-irq@hotmail.fr>
2023-03-20 09:28:01 -0700

    task 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'print alphabets lowercase and uppercase'
+ *Return: Always 0
+ */
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
