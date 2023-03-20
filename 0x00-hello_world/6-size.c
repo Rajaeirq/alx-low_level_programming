8ccac76b562d74540ddac224e277fc41c808fa58
Rajaeirq <rajae-irq@hotmail.fr>
2023-03-20 04:49:25 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
-* You should produce the exact same output as in the example
-* Warnings are allowed
+* main - print the string in the put function
 *
-* Your program should return 0
+* Description: using the main function
+* this program prints ""Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
