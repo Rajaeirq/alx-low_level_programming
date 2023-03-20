c5aea1d8110ebb524bb7a28cb50850ad86d45b0e
Rajaeirq <rajae-irq@hotmail.fr>
2023-03-20 04:37:04 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,16 @@
 */
 int main(void)
 {
-	char c;
-	int i;
-	longli;
-	long long lli;
-	float f;
+        char c;
+        int i;
+        long li;
+        long long lli;                         
+        float f;
 
-	print("size of a char: %ld byte(s)\n", sizeof(c));
-	print("size of a int: %ld byte(s)\n", sizeof(i));
-	print("size of a long: %ld byte(s)\n", sizeof(li));
-	print("size of a long long: %ld byte(s)\n", sizeof(lli));
-	print("size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
+        print("size of a char: %ld byte(s)\n", sizeof(c));
+        print("size of a int: %ld byte(s)\n", sizeof(i));
+        print("size of a long: %ld byte(s)\n", sizeof(li));
+        print("size of a long long: %ld byte(s)\n", sizeof(lli));
+        print("size of a float: %ld byte(s)\n", sizeof(f)); 
+        return (0);
 }
