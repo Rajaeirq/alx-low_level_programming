c8f3d0a947b30aea324724a5092e9faccec3eec8
Rajaeirq <rajae-irq@hotmail.fr>
2023-03-20 04:21:21 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main funstion 
+* Description: using the main function
 * this program prints with proper grammar, but the outcome is a piece of art,
 * Return: 0
-*/      
+*/
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
