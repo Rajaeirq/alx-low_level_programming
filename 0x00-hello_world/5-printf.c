9bc71297ad46759453377aa6b751d545dd022d4e
Rajaeirq <rajae-irq@hotmail.fr>
2023-03-20 04:15:31 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,10 @@
 *
 * Description: using the main funstion 
 * this program prints with proper grammar, but the outcome is a piece of art,
-* Return 0
-*/
+* Return: 0
+*/      
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
