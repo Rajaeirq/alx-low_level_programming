518863ca1f0bac94818ac4992ee8a722a125edf3
Rajaeirq <rajae-irq@hotmail.fr>
2023-03-20 05:11:38 -0700

    task7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 /**
 * main - print quote
 *
@@ -8,8 +9,8 @@
 * Return: Always 1 (Success)
 */
 
-inter main(void)
+int main(void)
 {
-write (2, "and that piece of art is useful" - Dora Korpar, 2015-10-1n", 59);
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
