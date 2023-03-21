45795d9ab8b6c00241f143f18fe589d2f852b608
Rajaeirq <rajae-irq@hotmail.fr>
2023-03-20 17:16:02 -0700

    task 101-101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
-
 /**
-* main - print quote
-*
-* Description: print Dora's quote
-* Return: Always 1 (Success)
-*/
-
+ * main - Entry
+ * Return: 1
+ */
 int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
